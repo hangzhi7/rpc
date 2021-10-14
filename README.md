7cc498ba3fdc13ed1d485f2a0ab1c23fc9657529
yuanhang <295043131@qq.com>
2021-10-14 14:20:44 +0800

    结构调整

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
-# rpc
+
+# simple-rpc
 
 ## 项目介绍
 简单实现rpc的服务端和客户端
 
 ## 结构
 ```text
-
+simple-rpc
+|-- rpc-demo            客户端
+|-- rpc-server-demo     服务端
+|-- rpc-service         业务包
 
 ```
 
---
+++ simple-rpc/rpc-server-demo/src/main/java/com/hh/rpc/utils/SpringContextUtil.java
@@ -0,0 +1,33 @@
+package com.hh.rpc.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * 获取上下文
+ *
+ * @author hang.yuan 2021/10/14 14:06
+ */
+
+@Component
+public class SpringContextUtil implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext contex)
+            throws BeansException {
+        SpringContextUtil.context = contex;
+    }
+
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+
+    public static Object getBean(String beanName) {
+        return context.getBean(beanName);
+    }
+
+}
